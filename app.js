5977507df77f81af224d7f6d3c2f6e8cae19017d
Malcolm Roosdahl <malcoroo@gmail.com>
2026-04-29 16:22:07 -0700

    added more functionality

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended: false}));
+
 //Routes
 app.get('/', (req, res) => {
     res.send(`
@@ -18,28 +20,58 @@ app.get('/login', (req, res) => {
     res.send(`
         <div>
             <h1>Login</h1>
-            <form action="post">
+            <form method="post" action="/loggingin">
                 <input type="text" name="username" id="username" placeholder="username"></input>
                 <input type="password" name="password" id="password" placeholder="password"></input>
-                <a href="/"><button>Login</button></a>
+                <button>Login</button>
             </form>
         </div>
         `);
 });
 
 app.get('/signup', (req, res) => {
+    var missingInput = req.query.missing;
+    var html = `
+    <div>
+        <h1>Sign up</h1>
+        <form method="post" action="/createAccount">
+            <input type="text" name="username" id="username" placeholder="username"></input>
+            <input type="email" name="email" id="email" placeholder="email"></input>
+            <input type="password" name="password" id="password" placeholder="password"></input>
+            <button>Submit</button>
+        </form>
+    </div>
+        `;
+
+    if(missingInput){
+        html += "</br> Please fill all inputs!"
+    }
+    res.send(html);
+});
+
+app.post('/loggingin', (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
     res.send(`
-        <div>
-            <h1>Sign up</h1>
-            <form action="post">
-                <input type="text" name="username" id="username" placeholder="username"></input>
-                <input type="email" name="email" id="email" placeholder="email"></input>
-                <input type="password" name="password" id="password" placeholder="password"></input>
-                <a href="/login"><button>Submit</button></a>
-            </form>
-            </div>
+        <p> user: ${username} pass: ${password} </p>
         `);
 });
+
+app.post('/createAccount', (req, res) => {
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    res.send(`
+        <p>user: ${username} email: ${email} pass: ${password}</p>
+        `);
+});
+
+app.use((req, res) => {
+    res.status(404);
+    res.send(`<h1>Page not Found - 404`);
+})
 
 //Start Server
 app.listen(PORT, () => {
