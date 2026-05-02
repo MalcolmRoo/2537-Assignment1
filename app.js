1613f6695f7ff27e043ca89cab711cdb562a6053
Malcolm Roosdahl <malcoroo@gmail.com>
2026-05-01 18:24:58 -0700

    added missing return

--- app.js
+++ app.js
@@ -179,6 +179,7 @@ app.post('/signupSubmit', async (req, res) => {
         req.session.cookie.maxAge = expireTime;
 
         res.redirect("/members");
+        return;
     }
 
     res.send(html);
