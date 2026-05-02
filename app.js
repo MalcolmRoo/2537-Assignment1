3b33ed406e08f7b12a9904f1e6111f385496e997
Malcolm Roosdahl <malcoroo@gmail.com>
2026-05-01 17:14:20 -0700

    removed problematic line for render hosting

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('node:dns/promises').setServers(['1.1.1.1', '8.8.8.8']);
+// require('node:dns/promises').setServers(['1.1.1.1', '8.8.8.8']);
 require("./utils.js");
 require('dotenv').config();
 const express = require('express');
